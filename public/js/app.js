64104b8f1ef26c101cb9b8636fe8158075a3551d
luoqingbo <luoqingbo@iot.chinamobile.com>
2017-03-15 14:47:19 +0800

    make:auth

--- public/js/app.js
+++ public/js/app.js
@@ -31620,7 +31620,7 @@ var Component = __webpack_require__(35)(
   /* cssModules */
   null
 )
-Component.options.__file = "E:\\a\\hm\\resources\\assets\\js\\components\\Example.vue"
+Component.options.__file = "E:\\a\\huimai\\resources\\assets\\js\\components\\Example.vue"
 if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key !== "__esModule"})) {console.error("named exports are not supported in *.vue files.")}
 if (Component.options.functional) {console.error("[vue-loader] Example.vue: functional components are not supported with templates, they should use render functions.")}
 
@@ -31631,9 +31631,9 @@ if (false) {(function () {
   if (!hotAPI.compatible) return
   module.hot.accept()
   if (!module.hot.data) {
-    hotAPI.createRecord("data-v-312bd321", Component.options)
+    hotAPI.createRecord("data-v-3bed2066", Component.options)
   } else {
-    hotAPI.reload("data-v-312bd321", Component.options)
+    hotAPI.reload("data-v-3bed2066", Component.options)
   }
 })()}
 
@@ -31718,7 +31718,7 @@ module.exports.render._withStripped = true
 if (false) {
   module.hot.accept()
   if (module.hot.data) {
-     require("vue-hot-reload-api").rerender("data-v-312bd321", module.exports)
+     require("vue-hot-reload-api").rerender("data-v-3bed2066", module.exports)
   }
 }
 
