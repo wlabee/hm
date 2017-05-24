acfbcc4f76758edcc611efa218fb2c49e923953c
luoqingbo <luoqingbo@iot.chinamobile.com>
2017-05-24 16:50:07 +0800

    left-cate

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -3,18 +3,20 @@
         <mheader></mheader>
         <router-view></router-view>
         <mfooter></mfooter>
+        <leftcate></leftcate>
     </div>
 </template>
 <script>
 import mheader from './layouts/Mheader'
 import menux from './layouts/Mmenu'
 import mfooter from './layouts/Mfooter'
+import leftcate from './layouts/LeftCate'
     export default {
         data() {
           return {
             sayhello: 'hello word'
           }
         },
-        components: {mheader, menux, mfooter}
+        components: {mheader, menux, mfooter, leftcate}
     }
 </script>
---
+++ resources/assets/js/components/layouts/LeftCate.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="left-cate bg-mtop">
+        <ul>
+            <li>小编精选</li>
+            <li>厨房用品</li>
+            <li>户外工具</li>
+            <li>卧室睡眠</li>
+            <li>步行运动</li>
+        </ul>
+    </div>
+</template>
+<style>
+    .left-cate {
+        position: absolute;
+        top: 500px;
+        right: 85%;
+    }
+    .left-cate ul {
+        padding-left: 0px;
+        margin-bottom: 0px;
+        width: 120px;
+        border: 4px solid #fd9e65;
+        background-color: #fff;
+        margin: 45px 0 0 45px;
+    }
+    .left-cate li {
+        list-style: none;
+        height: 40px;
+        line-height: 40px;
+        border-bottom: 1px solid #afafaf;
+        width: 100%;
+        text-align: center;
+    }
+    .bg-mtop {
+        background: url(/images/hm_tt1.png) no-repeat 80px 5px;
+    }
+</style>
